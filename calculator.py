276c5de97c9fb39987781912cd9a287cad9ba00e
akshayabelde <akshayabelde@gmail.com>
2024-07-16 13:24:54 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# Define a function to perform arithmetic operations
+def calculate(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+#user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operation (+, -, *, /): ")
+
+#calculation
+result = calculate(num1, num2, operator)
+
+#Display result
+print(f"Result: {result}")
